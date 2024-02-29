4d65fd882bdf9d092c88a4d5614dbbfd95520935
weeb js <yoitsjah.20@gmail.com>
2024-02-29 02:44:00 +0000

    loads of fixes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -18,22 +18,19 @@ import {
   UnstyledButton,
   MantineProvider,
   useComputedColorScheme,
-  useMantineColorScheme,
-  Affix,
-  Transition
+  useMantineColorScheme
 } from '@mantine/core';
 
-import classes from './Welcome.module.css';
-import '@mantine/spotlight/styles.css';
-import { useWindowScroll } from '@mantine/hooks';
-import { IconArrowUp } from '@tabler/icons-react';
+import classes from 'components/Welcome/Welcome.module.css';
 import { IconGauge, IconUser, IconCookie } from '@tabler/icons-react';
-import classes1 from './MobileNavbar.module.css';
+import classes1 from 'components/Welcome/MobileNavbar.module.css';
 import { FaSun, FaMoon } from 'react-icons/fa';
+import { IconExternalLink } from '@tabler/icons-react';
 
-export function Welcome() {
+const icon = <IconExternalLink size={14} />;
+
+export default function Welcome() {
   // Hook to control the color scheme
-  const [scroll, scrollTo] = useWindowScroll();
   const { setColorScheme } = useMantineColorScheme();
   const computedColorScheme = useComputedColorScheme('light');
   const toggleColorScheme = () => {
@@ -53,45 +50,51 @@ export function Welcome() {
               src="https://cdn.gilcdn.com/WebhookThumbnail/d56373e64d7ffcc3a9cd6871c6dc064d-Full.webp?w=160&h=160"
             />
             <Group ml="xl" gap={9} visibleFrom="sm" mr={40}>
-              <Button className={classes1.control} component='a' variant="default" href='https://zapguilded.vercel.app'>Home</Button>
-              <Button className={classes1.control} component='a' variant='default' href='/about'>About Us</Button>
-              <Button className={classes1.control} component='a' variant='default' href='https://twitter.com/zapguilded'>Twitter</Button>
-              <Button size='sm' variant='default' mb={25} onClick={toggleColorScheme}>
-                {computedColorScheme === "dark" ? <FaSun /> : <FaMoon />}
-              </Button>
+              <Button className={classes1.control} component='a' variant="default" href='https://zapguilded.vercel.app/'>Home</Button>
+              <Button className={classes1.control} component='a' variant='default' href='https://www.guilded.gg/b/2e702266-2dfe-4796-b61c-ccbb0536444c'>Invite</Button>
+              <Button className={classes1.control} component='a' variant='default' href='https://guilded.gg/i/knxezNqE'>Support</Button>
             </Group>
           </Group>
         </AppShell.Header>
       </AppShell>
       <Title className={classes.title} ta="left" mt={105}>
-        Hello! I'm{' '}
+        About{' '}
         <Text
           inherit
           variant="gradient"
           component="span"
           gradient={{ from: '#FFFF00', to: '#FFFF00' }}
         >
-          Zap
+          Us
         </Text>
       </Title>
       <Text className={classes.textLeft} c="dimmed" ta="left" size="lg" maw={580} mt="xl">
-        Zap, the ultimate multipurpose bot for safe and engaging online communities. This bot includes moderation,
-        reminders, and a lot more!{' '}
+        Zap is all about providing features for users on guilded to be accustomed with. Providing features such as Moderation, Anime, and more!
       </Text>
-      <Affix position={{ bottom: 20, right: 20 }}>
-        <Transition transition="slide-up" mounted={scroll.y > 0}>
-          {(transitionStyles) => (
-            <Button
-              leftSection={<IconArrowUp style={{ width: rem(16), height: rem(16) }} />}
-              style={transitionStyles}
-              onClick={() => scrollTo({ y: 0 })}
-              variant='default'
-            >
-              Scroll to top
-            </Button>
-          )}
-        </Transition>
-      </Affix>
+      <Group className="column" align="start" mt={32} ml={50}>
+        <Button
+          justify="center"
+          component="a"
+          href="https://www.guilded.gg/b/2e702266-2dfe-4796-b61c-ccbb0536444c"
+          size="lg"
+          rightSection={icon}
+          variant="default"
+          mt="md"
+        >
+          Invite Zap
+        </Button>
+        <Button
+          justify="center"
+          component="a"
+          href="https://www.guilded.gg/i/knxezNqE"
+          size="lg"
+          rightSection={icon}
+          variant="default"
+          mt="md"
+        >
+          Support Server
+        </Button>
+      </Group>
     </MantineProvider>
   );
 }
