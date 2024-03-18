7ee7587e9eb6e905cdbc7b6451bffad2abfd802c
weeb js <yoitsjah.20@gmail.com>
2024-03-18 21:08:31 +0000

    fixed stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Welcome } from '../components/Welcome/Welcome';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
-import { AppShell } from '@mantine/core';
+import { AppShell, MantineProvider } from '@mantine/core';
 import classes from './Welcome.module.css';
 import { IconPhoto } from '@tabler/icons-react';
 
@@ -8,6 +8,7 @@ export default function HomePage() {
   return (
     <>
       <Welcome />
+      <MantineProvider defaultColorScheme="dark"></MantineProvider>
       <ColorSchemeToggle />
     </>
   );
